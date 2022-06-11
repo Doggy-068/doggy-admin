c600dc60be6d58f589535af2c98e0e131168a4fc
Doggy-068 <728724654@qq.com>
2022-06-11 21:21:14 +0800

    chore: hash

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { ConfigProvider } from 'antd'
 import lang from './lang'
 import 'antd/dist/antd.css'
@@ -9,10 +9,10 @@ import './main.scss'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <ConfigProvider locale={lang.antd}>
         <App />
       </ConfigProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 )
